5cfdd6315478be58c1fb30919ff4923fcdf27ddf
Jake Brown <jake@eyespacelenses.com>
2020-08-17 10:37:34 +0930

    styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,43 @@
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-  }
-  .list {
-    width: 200px;
-    margin-left: auto;
-    margin-right: auto;
-    display: block;
-  }
-
   h1 {
-    color: #ff3e00;
     text-transform: uppercase;
-    font-size: 4em;
+    font-size: 3em;
     font-weight: 100;
   }
 
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  main {
+    text-align: center;
+    height: 100vh;
+  }
+
+  p {
+    font-size: 1.2em;
+  }
+
+  .container {
+    display: flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+    align-content: center;
+  }
+
+  .el {
+    margin: 5px;
+    margin-bottom: 5px;
+    background: black;
+    color: white;
+    font-size: 0.9em;
+    width: 180px;
+    height: 180px;
+    text-align: center;
+    text-transform: uppercase;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .el:nth-child(2) {
+    background: red;
+    height: 200px;
   }
 </style>
 
@@ -30,11 +46,19 @@
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <div class="list">
-    <p>This is boilerplate with the following configs:</p>
-    <ul>
-      <li>Normalize.css</li>
-    </ul>
+  <h1>Svelte Min</h1>
+  <p>
+    This is a boilerplate Svelte template. It is designed to quickly spin up an
+    Svelte app and begin writing HTML/CSS/JS with the following:
+  </p>
+  <div class="container">
+    <div class="el">Normalize.css</div>
+    <div class="el">
+      <span>Prettier<br>(and RC file)</span>
+    </div>
+    <div class="el">
+      <span>Flex layout</span>
+    </div>
   </div>
+  <p>No global stylesheet and no client-side routing.</p>
 </main>
